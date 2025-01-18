91621ac4586108eefe211f77293ff06c147fb2f5
Gamshik <122757204+Gamshik@users.noreply.github.com>
2025-01-18 04:05:34 +0300

    Add commen, bugifx and redactorung

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,9 @@
 #include <QGridLayout>
 #include <QTextEdit>
 #include <QPushButton>
+#include <QVBoxLayout>
+#include <QLabel>
+#include <QMessageBox>
 
 #include <iostream>
 #include <fstream>
@@ -15,19 +18,33 @@ namespace fs = std::filesystem;
 
 MainWindow *mainWnd;
 
-QGridLayout *mainGridLayout;
+// ----- LAYOUTS -----
+
+QGridLayout *mainLayout;
+QGridLayout *projNameInputLayout;
+QGridLayout *projRootDirInputLayout;
+
+// ----- COMPONENTS -----
+
 QPushButton *createProjBtn;
 QTextEdit *projNameTxtEditor;
 QTextEdit *projRootDirTxtEditor;
+
+// ----- ON EVENT FUNCTIONS -----
 
 void onCreateProjBtnClick();
 void onProjRootDirTxtEditorChange();
 void onProjNameTxtEditorChange();
 
+// ---- FUNCTIONS -----
+
 string readFile(string path);
 void createFile(string path, string content);
 void createParentDirectoriesIfNotExist(string path);
 string setVariableInText(string varName, string varPattern, string varVal, string txt);
+QString strToQString(string str);
+
+// ----- CONSTANTS -----
 
 const string fileInstancesDirPath = "../../../file_instances/";
 
@@ -42,39 +59,82 @@ const string moduleInstanceFilePath = fileInstancesDirPath + "app.module.ts.txt"
 
 const string mainInstanceFilePath = fileInstancesDirPath + "main.ts.txt";
 
+// ----- VARIABLES -----
 
 QString projRootDirName;
 QString projName;
 
+
+
 int main(int argc, char *argv[])
 {
+    // -------------------------- INIT/CREATE --------------------------
     QApplication a(argc, argv);
 
     QSize wndSize{600, 500};
 
     mainWnd = new MainWindow(wndSize, "Js Low Code");
 
-    mainGridLayout = new QGridLayout(mainWnd);
+    mainLayout = new QGridLayout(mainWnd);
+    projNameInputLayout = new QGridLayout();
+    projRootDirInputLayout = new QGridLayout();
+
+    QLabel *projNameInputLabel = new QLabel(QString::fromStdString("Project name*:"));
+    QLabel *projRootDirInputLabel = new QLabel(QString::fromStdString("Project root directory name*:"));
 
     createProjBtn = new QPushButton("Ctreate proj");
 
     projRootDirTxtEditor = new QTextEdit();
 
-    projRootDirTxtEditor->setMaximumHeight(30);
-    projRootDirTxtEditor->setMaximumWidth(200);
-
     projNameTxtEditor = new QTextEdit();
 
-    projNameTxtEditor->setMaximumHeight(30);
-    projNameTxtEditor->setMaximumWidth(200);
+
+
+    // -------------------------- ACTION BINDING --------------------------
 
     QObject::connect(createProjBtn, QPushButton::clicked, onCreateProjBtnClick);
     QObject::connect(projNameTxtEditor, QTextEdit::textChanged, onProjNameTxtEditorChange);
     QObject::connect(projRootDirTxtEditor, QTextEdit::textChanged, onProjRootDirTxtEditorChange);
 
-    mainGridLayout->addWidget(projNameTxtEditor);
-    mainGridLayout->addWidget(projRootDirTxtEditor);
-    mainGridLayout->addWidget(createProjBtn);
+
+
+    // -------------------------- VIEW STYLE --------------------------
+
+    projNameInputLabel->setMaximumHeight(30);
+
+    projNameTxtEditor->setMaximumHeight(30);
+    projNameTxtEditor->setMaximumWidth(200);
+
+    projNameInputLayout->setVerticalSpacing(15);
+
+    projRootDirInputLabel->setMaximumHeight(30);
+
+    projRootDirTxtEditor->setMaximumHeight(30);
+    projRootDirTxtEditor->setMaximumWidth(200);
+
+    projRootDirInputLayout->setVerticalSpacing(15);
+
+    createProjBtn->setMinimumHeight(30);
+
+    // mainWnd->setStyleSheet("QWidget {border : 2px solid red}");
+
+
+
+    // -------------------------- PLACEMENT ELEMENTS --------------------------
+
+    projNameInputLayout->addWidget(projNameInputLabel);
+    projNameInputLayout->addWidget(projNameTxtEditor);
+
+    projRootDirInputLayout->addWidget(projRootDirInputLabel);
+    projRootDirInputLayout->addWidget(projRootDirTxtEditor);
+
+    mainLayout->addLayout(projNameInputLayout, 0, 0);
+    mainLayout->addLayout(projRootDirInputLayout, 1, 0);
+    mainLayout->addWidget(createProjBtn, 2, 0);
+
+
+
+    // -------------------------- START --------------------------
 
     mainWnd->show();
 
@@ -83,47 +143,65 @@ int main(int argc, char *argv[])
 
 void onCreateProjBtnClick()
 {
-    if (projRootDirName.length() < 1)
+    QMessageBox msgBox;
+
+    if (projName.length() < 1 || projRootDirName.length() < 1)
+    {
+        msgBox.setText("All field is required");
+        msgBox.exec();
+
         return;
 
-    string packageJsonConfFileContent = readFile(packageJsonInstanceFilePath);
-    string tsConfFileContent = readFile(tsConfInstanceFilePath);
-    string tsConfBuildFileContent = readFile(tsConfBuildInstanceFilePath);
-    string nestCliConfFileContent = readFile(nestCliConfInstanceFilePath);
+    }
 
-    string serviceFileContent = readFile(serviceInstanceFilePath);
-    string controllerFileContent = readFile(serviceInstanceFilePath);
-    string moduleFileContent = readFile(serviceInstanceFilePath);
+    try
+    {
+        string packageJsonConfFileContent = readFile(packageJsonInstanceFilePath);
+        string tsConfFileContent = readFile(tsConfInstanceFilePath);
+        string tsConfBuildFileContent = readFile(tsConfBuildInstanceFilePath);
+        string nestCliConfFileContent = readFile(nestCliConfInstanceFilePath);
 
-    string mainFileContent = readFile(mainInstanceFilePath);
+        string serviceFileContent = readFile(serviceInstanceFilePath);
+        string controllerFileContent = readFile(controllerInstanceFilePath);
+        string moduleFileContent = readFile(moduleInstanceFilePath);
 
-    packageJsonConfFileContent = setVariableInText("projName", "{%v}", projName.toStdString(), packageJsonConfFileContent);
-    packageJsonConfFileContent = setVariableInText("rootDir", "{%v}", projRootDirName.toStdString(), packageJsonConfFileContent);
-    nestCliConfFileContent = setVariableInText("rootDir", "{%v}", projRootDirName.toStdString(), nestCliConfFileContent);
+        string mainFileContent = readFile(mainInstanceFilePath);
 
-    string projFolderPath = "../../../" + projName.toStdString() + "/";
-    string projRootDirPath = projFolderPath + projRootDirName.toStdString() + "/";
+        packageJsonConfFileContent = setVariableInText("projName", "{%v}", projName.toStdString(), packageJsonConfFileContent);
+        packageJsonConfFileContent = setVariableInText("rootDir", "{%v}", projRootDirName.toStdString(), packageJsonConfFileContent);
+        nestCliConfFileContent = setVariableInText("rootDir", "{%v}", projRootDirName.toStdString(), nestCliConfFileContent);
 
-    string serviceFilePath = projRootDirPath + "app.service.ts";
-    string controllerFilePath = projRootDirPath + "app.controller.ts";
-    string moduleFilePath = projRootDirPath + "app.module.ts";
-    string mainFilePath = projRootDirPath + "main.ts";
+        string projFolderPath = "../../../" + projName.toStdString() + "/";
+        string projRootDirPath = projFolderPath + projRootDirName.toStdString() + "/";
 
-    string packageJsonConfFilePath = projFolderPath + "package.json";
-    string tsConfFilePath = projFolderPath + "tsconfig.json";
-    string tsConfBuildFilePath = projFolderPath + "tsconfig.build.json";
-    string nestCliConfFilePath = projFolderPath + "nest-cli.json";
+        string serviceFilePath = projRootDirPath + "app.service.ts";
+        string controllerFilePath = projRootDirPath + "app.controller.ts";
+        string moduleFilePath = projRootDirPath + "app.module.ts";
+        string mainFilePath = projRootDirPath + "main.ts";
 
-    createFile(serviceFilePath, serviceFileContent);
-    createFile(controllerFilePath, controllerFileContent);
-    createFile(moduleFilePath, moduleFileContent);
+        string packageJsonConfFilePath = projFolderPath + "package.json";
+        string tsConfFilePath = projFolderPath + "tsconfig.json";
+        string tsConfBuildFilePath = projFolderPath + "tsconfig.build.json";
+        string nestCliConfFilePath = projFolderPath + "nest-cli.json";
 
-    createFile(mainFilePath, mainFileContent);
+        createFile(serviceFilePath, serviceFileContent);
+        createFile(controllerFilePath, controllerFileContent);
+        createFile(moduleFilePath, moduleFileContent);
 
-    createFile(packageJsonConfFilePath, packageJsonConfFileContent);
-    createFile(tsConfFilePath, tsConfFileContent);
-    createFile(tsConfBuildFilePath, tsConfBuildFileContent);
-    createFile(nestCliConfFilePath, nestCliConfFileContent);
+        createFile(mainFilePath, mainFileContent);
+
+        createFile(packageJsonConfFilePath, packageJsonConfFileContent);
+        createFile(tsConfFilePath, tsConfFileContent);
+        createFile(tsConfBuildFilePath, tsConfBuildFileContent);
+        createFile(nestCliConfFilePath, nestCliConfFileContent);
+
+        msgBox.setText(strToQString("Project " + projName.toStdString() + " is created"));
+        msgBox.exec();
+    } catch(exception &exception)
+    {
+        msgBox.setText(QString("Something went wront: %1").arg(exception.what()));
+        msgBox.exec();
+    }
 }
 
 void onProjRootDirTxtEditorChange()
@@ -191,4 +269,9 @@ string setVariableInText(string varName, string varPattern, string varVal, strin
     qTxt.replace(pattern, QString::fromStdString(varVal));
 
     return qTxt.toStdString();
+}
+
+QString strToQString(string str)
+{
+    return QString::fromStdString(str);
 }
