7ea810c007bf550266c958f9d891c358880a2f55
Gamshik <122757204+Gamshik@users.noreply.github.com>
2025-01-26 16:46:03 +0300

    Add logic for set service variables to file

--- src/nestJsServiceFile.h
+++ src/nestJsServiceFile.h
@@ -3,15 +3,34 @@
 
 #include "iostream"
 
-#include <iostream>
+#include "nestJsProjectFile.h"
+#include "fileSystemProvider.h"
+#include "stringHelper.h"
 
 using namespace std;
 
 class NestJsServiceFile : public NestJsProjectFile
 {
+private:
+    const string NAME_VARIABLE_PATTERN = "{serviceName}";
+
+    string _name;
+
 public:
-    NestJsServiceFile(): NestJsProjectFile("service.ts.txt") {}
+    NestJsServiceFile(string name):
+        _name(name),
+        NestJsProjectFile("service.ts.txt")
+    {}
     virtual ~NestJsServiceFile() {}
+
+    void createFile(string path)
+    {
+        string fileContent = FileSystemProvider::readFile(INSTANCE_FILE_PATH);
+
+        fileContent = StringHelper::setVariableInText(NAME_VARIABLE_PATTERN, _name, fileContent);
+
+        FileSystemProvider::createFile(path, fileContent);
+    }
 };
 
 #endif // NESTJSSERVICEFILE_H
