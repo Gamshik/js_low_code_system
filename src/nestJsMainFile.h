4fa55f758b004474d2c4a5989ea253b5c7bc80d6
Gamshik <122757204+Gamshik@users.noreply.github.com>
2025-01-26 16:50:43 +0300

    Add main module to create project logic

--- src/nestJsMainFile.h
+++ src/nestJsMainFile.h
@@ -2,6 +2,7 @@
 #define NESTJSMAINFILE_H
 
 #include "nestJsProjectFile.h"
+#include "stringHelper.h"
 
 #include "iostream"
 
@@ -9,9 +10,35 @@ using namespace std;
 
 class NestJsMainFile : public NestJsProjectFile
 {
+private:
+    const string MAIN_MODULE_NAME_VARIABLE_PATTERN = "{mainModuleName}";
+    const string MAIN_MODULE_NAME_CAMEL_CASE_VARIABLE_PATTERN = "{mainModuleNameCamelCase}";
+
+    string _mainModuleName;
+    string _mainModuleNameCamelCase;
+
 public:
-    NestJsMainFile(): NestJsProjectFile("main.ts.txt") {}
+    NestJsMainFile():
+        _mainModuleName("Main"),
+        _mainModuleNameCamelCase(StringHelper::toLowerCaseFirstLetter(_mainModuleName)),
+        NestJsProjectFile("main.ts.txt")
+    {}
+    NestJsMainFile(string moduleName):
+        _mainModuleName(moduleName),
+        _mainModuleNameCamelCase(StringHelper::toLowerCaseFirstLetter(moduleName)),
+        NestJsProjectFile("main.ts.txt")
+    {}
     virtual ~NestJsMainFile() {}
+
+    void createFile(string path)
+    {
+        string fileContent = FileSystemProvider::readFile(INSTANCE_FILE_PATH);
+
+        fileContent = StringHelper::setVariableInText(MAIN_MODULE_NAME_VARIABLE_PATTERN, _mainModuleName, fileContent);
+        fileContent = StringHelper::setVariableInText(MAIN_MODULE_NAME_CAMEL_CASE_VARIABLE_PATTERN, _mainModuleNameCamelCase, fileContent);
+
+        FileSystemProvider::createFile(path, fileContent);
+    }
 };
 
 #endif // NESTJSMAINFILE_H
--- src/nestJsProjectCreator.h
+++ src/nestJsProjectCreator.h
@@ -7,6 +7,7 @@
 #include "nestJsCliConfigFile.h"
 #include "nestJsModule.h"
 #include "nestJsMainFile.h"
+#include "nestJsMainModuleFile.h"
 
 #include "iostream"
 
@@ -19,16 +20,17 @@ private:
     NestJsTsConfigFile *_tsConfigFile;
     NestJsTsConfigBuildFile *_tsConfigBuildFile;
     NestJsCliConfigFile *_cliConfigFile;
+    NestJsMainModuleFile *_mainModuleFile;
     NestJsMainFile *_mainFile;
 
     const string PACKAGE_FILE_PATH;
     const string TS_CONFIF_FILE_PATH;
     const string TS_CONFIG_BUILD_FILE_PATH;
     const string CLI_CONFIG_FILE_PATH;
+    const string MAIN_MODULE_FILE_PATH;
     const string MAIN_FILE_PATH;
 
     vector<NestJsModule*> _modules;
-    int _countModules;
 
     string _projRootDirPath;
 
@@ -38,14 +40,15 @@ public:
         TS_CONFIF_FILE_PATH(projPath + "/" + projName + "/tsconfig.json"),
         TS_CONFIG_BUILD_FILE_PATH(projPath + "/" + projName + "/tsconfig.build.json"),
         CLI_CONFIG_FILE_PATH(projPath + "/" + projName + "/nest-cli.json"),
+        MAIN_MODULE_FILE_PATH(projPath + "/" + projName + "/" + rootDir + "/main.module.ts"),
         MAIN_FILE_PATH(projPath + "/" + projName + "/" + rootDir + "/main.ts"),
         _packageFile(new NestJsPackageFile(projName, rootDir)),
         _tsConfigFile(new NestJsTsConfigFile()),
         _tsConfigBuildFile(new NestJsTsConfigBuildFile()),
         _cliConfigFile(new NestJsCliConfigFile(rootDir)),
         _mainFile(new NestJsMainFile()),
+        _mainModuleFile(new NestJsMainModuleFile("Main")),
         _modules(0),
-        _countModules(0),
         _projRootDirPath(projPath + "/" + projName + "/" + rootDir)
     {}
     virtual ~NestJsProjectCreator()
@@ -55,11 +58,13 @@ public:
         delete _tsConfigBuildFile;
         delete _cliConfigFile;
         delete _mainFile;
+        delete _mainModuleFile;
     }
 
     void addModule(string name)
     {
         _modules.push_back(new NestJsModule(name, _projRootDirPath));
+        _mainModuleFile->addModule(name);
     }
     void createProject()
     {
@@ -67,6 +72,7 @@ public:
         _tsConfigFile->createFile(TS_CONFIF_FILE_PATH);
         _tsConfigBuildFile->createFile(TS_CONFIG_BUILD_FILE_PATH);
         _cliConfigFile->createFile(CLI_CONFIG_FILE_PATH);
+        _mainModuleFile->createFile(MAIN_MODULE_FILE_PATH);
         _mainFile->createFile(MAIN_FILE_PATH);
 
         for (NestJsModule *item : _modules)
